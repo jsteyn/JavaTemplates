15f208c51dab33aea80eff24b294b75347e11289
Jannetta Steyn <6432530+jsteyn@users.noreply.github.com>
2023-06-16 09:29:51 +0100

    Restructure in modules to add pom file to each example

--- FileIO/src/main/java/fileio/FileDirectoryExistsExample.java
+++ FileIO/src/main/java/fileio/FileDirectoryExistsExample.java
@@ -2,19 +2,33 @@ package fileio;
 
 import java.io.File;
 
+/**
+ * <ol>
+ * <li>How to check whether a file or a directory exists or not</li>
+ * <li>How to check whether something is a file or a directory</li>
+ * </ol>
+ */
 public class FileDirectoryExistsExample {
     static File directory = new File("sqlite/");
     static File file = new File("sqlite/sqlite.properties");
 
     public static void main(String[] args) {
         // test "/var/tmp" directory
+
+        // check to see if given exists
         boolean exists = directory.exists();
         if (exists) {
             System.out.println(directory.getAbsolutePath() + " exists");
         } else {
             System.out.println(directory.getAbsolutePath() + " does not exist");
         }
+        if (file.getAbsoluteFile().exists()) {
+            System.out.println(file.getAbsoluteFile() + " exists");
+        } else {
+            System.out.println(file.getAbsolutePath() + " does not exists");
+        }
 
+        // test to see if given is a directory
         if (directory.isDirectory()) System.out.println(directory.getAbsolutePath() + " is a directory");
 
         // test to see if a file exists
